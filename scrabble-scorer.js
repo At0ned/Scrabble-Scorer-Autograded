3a0ca8b82e4f1366f946f57bf24f3bb681626187
At0ned <sethaholt@gmail.com>
2024-04-11 16:12:42 -0500

    Before making npm tests work

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -3,74 +3,74 @@
 const input = require("readline-sync");
 
 const oldPointStructure = {
-  1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-  2: ['D', 'G'],
-  3: ['B', 'C', 'M', 'P'],
-  4: ['F', 'H', 'V', 'W', 'Y'],
-  5: ['K'],
-  8: ['J', 'X'],
-  10: ['Q', 'Z']
+   1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+   2: ['D', 'G'],
+   3: ['B', 'C', 'M', 'P'],
+   4: ['F', 'H', 'V', 'W', 'Y'],
+   5: ['K'],
+   8: ['J', 'X'],
+   10: ['Q', 'Z']
 };
 
 function oldScrabbleScorer(word) {
-	word = word.toUpperCase();
-	let letterPoints = "";
- 
-	for (let i = 0; i < word.length; i++) {
- 
-	  for (const pointValue in oldPointStructure) {
- 
-		 if (oldPointStructure[pointValue].includes(word[i])) {
-			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
-		 }
- 
-	  }
-	}
-	return letterPoints;
- }
+   word = word.toUpperCase();
+   let letterPoints = "";
+
+   for (let i = 0; i < word.length; i++) {
+
+      for (const pointValue in oldPointStructure) {
+
+         if (oldPointStructure[pointValue].includes(word[i])) {
+            letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+         }
+
+      }
+   }
+   return letterPoints;
+}
 
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
 // you need to uncomment in run program. figure out transform function, figure out how to get the initial prompt output into the function scorerprompt
 
-let word;
- function initialPrompt(word) {
-  word = input.question("Let's play some scrabble! Enter a word: ");
-  if (!isNaN(Number(word))) {
-   initialPrompt()
-}
+
+function initialPrompt() {
+   let word = input.question("Let's play some scrabble! Enter a word: ");
+
+   if (!isNaN(Number(word))) {
+      initialPrompt()
+   }
    return word;
 };
 
- word = initialPrompt(word)
 
 
-let simpleScorer =   {
+let simpleScorer = {
    name: "Simple score",
    description: "Each letter is worth 1 point",
-   Score: function () {
-let score = (word.length);
-return console.log(score);
-}
+   Score: function (word) {
+      let score = (word.length);
+      return score;
+   }
 };
-let vowelBonusScorer = { 
+let vowelBonusScorer = {
    name: "Vowel Bonus Score",
    description: "Vowels are 3 pts, consonants are 1 pt.",
    Score: function (word) {
-   let score = 0;
-   let i = 0;
-   let realWord = word.toUpperCase()
-   while (i < realWord.length){
-   if (realWord[i] == "A" || realWord[i] == "E" || realWord[i] == "I" || realWord[i] == "O" || realWord[i] == "U") {
-      score += 3
-   } else {
-      score++
+      let score = 0;
+      let i = 0;
+      let realWord = word.toUpperCase()
+      while (i < realWord.length) {
+         if (realWord[i] == "A" || realWord[i] == "E" || realWord[i] == "I" || realWord[i] == "O" || realWord[i] == "U") {
+            score += 3
+         } else {
+            score++
+         }
+         i++
+      }
+      return score
    }
-   i++
-}
-   return console.log(score)
-}
 };
 let scrabbleScorer = {
    name: "Scrabble",
@@ -82,35 +82,35 @@ const scoringAlgorithms = [simpleScorer, vowelBonusScorer, scrabbleScorer];
 
 function scorerPrompt() {
    let choice = input.question(`Which scoring algorithm do you want?\n\n 0 - ${simpleScorer.name} - ${simpleScorer.description}\n 1 - ${vowelBonusScorer.name} - ${vowelBonusScorer.description}\n 2 - ${scrabbleScorer.name} - ${scrabbleScorer.description}\nEnter 0, 1, or 2: `)
-   
-  let chosenObject;
-  
-   
-   
+
+   let chosenObject;
+   //scoringalgorithms[0]... make more efficient
+
    if (choice === "0") {
-      chosenObject = simpleScorer.Score(word);
-    
+      chosenObject = simpleScorer;
+
    }
    else if (choice === "1") {
-      chosenObject = vowelBonusScorer.Score(word);
-      
-   } else if (choice === "2")  {
-      chosenObject = scrabbleScorer.Score(word);
-      
+      chosenObject = vowelBonusScorer;
+
+   } else if (choice === "2") {
+      chosenObject = scrabbleScorer;
+
    } else {
       return scorerPrompt();
    }
 
-   return console.log(`The total for ${word} is ${chosenObject}`) ;
+   return chosenObject;
 };
-   
-   
+// chosenObject = scorerPrompt()
+// console.log(chosenObject.Score(initialPrompt(word)));
+
 
 function transform(oldPointStructure) {
    let brandNew = {};
    for (oldPointStructure["2"] in oldPointStructure) {
       if (oldPointStructure.includes())
-      brandNew["a"] = (oldPointStructure[1][0])
+         brandNew["a"] = (oldPointStructure[1][0])
    }
    return console.log(brandNew)
 };
@@ -118,10 +118,11 @@ function transform(oldPointStructure) {
 let newPointStructure;
 
 function runProgram() {
-   initialPrompt();
-   scorerPrompt();
+   const word = initialPrompt();
+   const chosenObject = scorerPrompt();
+   console.log(chosenObject.Score(word));
    //transform(oldPointStructure);
-   
+
 }
 
 
@@ -136,8 +137,7 @@ module.exports = {
    scrabbleScorer: scrabbleScorer,
    scoringAlgorithms: scoringAlgorithms,
    newPointStructure: newPointStructure,
-	runProgram: runProgram,
-	scorerPrompt: scorerPrompt
+   runProgram: runProgram,
+   scorerPrompt: scorerPrompt
 };
 
-// with this commit, the input is not being registered in the object
